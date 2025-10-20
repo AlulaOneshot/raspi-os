9c4f0ffc31d597401791cbbba7c7da0e7677b26a
Liam Greenway <liamgr33nway@gmail.com>
2025-10-20 10:22:11 -0400

    Update

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 git clone https://github.com/raspberrypi/linux --depth 1 -b rpi-6.12.y
-cd linux
 
-export KERNEL_DIR=$PWD
-export ARCH=arm64
-export CROSS_COMPILE=aarch64-linux-gnu-
-make bcm2711_defconfig
+cd linux
+KERNEL=kernel8
+make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- bcm2711_defconfig
+cp ../.config .config
+make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- Image modules dtbs
