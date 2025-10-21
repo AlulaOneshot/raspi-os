e9dab5aeefa44de22b3389daa3a243029da6177b
Liam Greenway <liamgr33nway@gmail.com>
2025-10-21 15:52:34 +0000

    Update

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ set -e
 wget https://buildroot.org/downloads/buildroot-2025.08.tar.xz
 tar -xf buildroot-2025.08.tar.xz
 cd buildroot-2025.08
-make config
+make raspberrpi4_64_defconfig
+cp ../.config .config
