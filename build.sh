45613b2475f87809a324c06aea958ea881ab2566
Liam Greenway <liam.greenway@students.coderva.org>
2025-10-21 11:30:38 +0000

    Update

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
+set -e
+
 wget https://buildroot.org/downloads/buildroot-2025.08.tar.xz
 tar -xf buildroot-2025.08.tar.xz
-cp .config buildroot-2025.08/.config
 cd buildroot-2025.08
-make
+make BR2_EXTERNAL=../.config
