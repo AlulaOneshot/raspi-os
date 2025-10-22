b55e6e0449a489f11ddcacdc79a74ee162726480
Liam Greenway <liam.greenway@students.coderva.org>
2025-10-22 08:55:20 +0000

    Update

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@ set -e
 wget https://buildroot.org/downloads/buildroot-2025.08.tar.xz
 tar -xf buildroot-2025.08.tar.xz
 cd buildroot-2025.08
-make raspberrpi4_64_defconfig
-cp ../.config .config
+cp ../shiota_devkit_defconfig configs/shiota_devkit_defconfig
+make shiota_devkit_defconfig
+make -j$(nproc)
